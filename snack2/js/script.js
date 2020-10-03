abdfc854076a575ea4b6b7d755393a2674d1905d
Flavia <flaviamatta90@gmail.com>
2020-10-03 12:49:11 +0200

    Snack 2 split reverse join

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -1,2 +1,13 @@
-snack2
+// snack2
 // Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
+
+function reverse(word) {
+    var splitArray = word.split("");
+
+    var reverseArray = splitArray.reverse();
+
+    var joinArray = reverseArray.join("");
+
+    return joinArray;
+}
+console.log(reverse("CIAO"));
