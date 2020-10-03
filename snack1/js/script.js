28806af978e652d5d4e8bdcc56d1cc7118d315ea
Flavia <flaviamatta90@gmail.com>
2020-10-03 12:14:10 +0200

    snack 1

---
+++ snack1/js/script.js
@@ -0,0 +1,21 @@
+// snack1
+
+// Fai inserire un numero, che chiameremo N, all’utente.
+var n = parseInt(prompt("Inserisci un numero"));
+
+// Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
+// funzione standard per i numeri random
+function random(min, max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// numeri scelti dall'utente + numeri casuali
+for (var i = 0; i < n; i++) {
+  var arrayVuoto = [];
+  for (var y = 0; y < 10; y++){
+    arrayVuoto.push(random(1,100));
+  }
+}
+
+// Ogni volta che ne crei uno, stampalo a schermo.
+document.write("il numero scelto dall'utente --> " + n + " " + "e i numeri random da 1 a 100 --> " + arrayVuoto);
